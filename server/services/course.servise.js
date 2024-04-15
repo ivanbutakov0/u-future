561167f93283cf9c5dabb6d2e0d68ab4e080f3d4
Ivan Butakov <ivanbutakov000@gmail.com>
2024-04-15 13:10:45 +0500

    add create course endpoint

--- server/services/course.servise.js
+++ server/services/course.servise.js
@@ -1,20 +1,9 @@
 const Course = require('../models/course.model')
-const { errorHandler } = require('../utils/error')
 
 const createCourseService = async (title, userId) => {
-	const category = await Course.findOne({ _id: categoryId })
-
-	if (!category) {
-		return errorHandler(404, 'Категория не найдена')
-	}
-
 	const course = await Course.create({
 		title,
-		description,
-		imageUrl,
-		price,
 		userId,
-		categoryId,
 	})
 	return course
 }
