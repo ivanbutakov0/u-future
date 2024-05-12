13810b4aed752ed9244f09eeaa85c50abb9a93b7
Ivan Butakov <ivanbutakov000@gmail.com>
2024-05-12 12:35:23 +0500

    fix create chapter functionality

--- server/controllers/chapter.controller.js
+++ server/controllers/chapter.controller.js
@@ -1,4 +1,5 @@
 const { createChapterService } = require('../services/chapter.servise')
+const { getCourseService } = require('../services/course.servise')
 
 const createChapter = async (req, res, next) => {
 	try {
@@ -6,6 +7,12 @@ const createChapter = async (req, res, next) => {
 
 		if (!title || !courseId) {
 			throw new Error('Title and course id are required')
+		}
+
+		const course = await getCourseService(courseId)
+
+		if (!course) {
+			throw new Error('Course not found')
 		}
 
 		const newChapter = await createChapterService(title, courseId)
--- server/services/chapter.servise.js
+++ server/services/chapter.servise.js
@@ -1,5 +1,7 @@
+const Chapter = require('../models/chapter.model')
+
 const createChapterService = async (title, courseId) => {
-	return await Chapter.create({ title, courseId })
+	return await Chapter.create({ title, course: courseId })
 }
 
 module.exports = { createChapterService }
