5dd81f121294d9ff6629107191dd8d7248f6ac0f
Ivan Butakov <ivanbutakov000@gmail.com>
2024-06-05 12:41:18 +0500

    add money field to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -26,6 +26,10 @@ const UserSchema = new mongoose.Schema(
 			type: [mongoose.Schema.Types.ObjectId],
 			ref: 'Course',
 		},
+		money: {
+			type: Number,
+			default: 0,
+		},
 	},
 	{ timestamps: true }
 )
